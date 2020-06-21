df0882fb3db75b486414bfc1bb591b27e2515838
shubham kushwaha <shubhkush1234@gmail.com>
2020-06-21 21:49:37 +0530

    setState and useState updated

--- src/App.js
+++ src/App.js
@@ -2,39 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+function App() {
 
-  state= {
-    Counter: 0
-  }
-
-  incCounter = (state) => {
-    this.setState({
-      Counter: this.state.Counter + 1
+  const incCounter = (state) => {
+    state({
+      counter: state.counter + 1
     });
   }
 
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-    <button onClick={this.incCounter}>I am clicked {this.state.Counter}</button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <button onClick={incCounter}>I am clicked {incCounter}</button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
           </a>
-        </header>
-      </div>
-    );
-  }
-  
+      </header>
+    </div>
+  );
 }
 
 export default App;
