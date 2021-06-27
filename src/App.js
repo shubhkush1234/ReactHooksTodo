8acb2dc17fe785b3f9a5016c2012b32d4e7c58f3
shubham kushwaha <shubhkush1234@gmail.com>
2021-06-27 14:07:56 +0530

    props drilling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App = () => {
-
-  const [count, setCount] = useState(0);
-  const [isOn, SetIsOn] = useState(false);
-
-  const incCounter = (state) => {
-    setCount(count + 1);
-    console.log(count);
-  }
-
-  const styless = {
-    backgroundColor: "black",
-    height: "50px",
-    width: "50px"
-  }
+const App = (props) => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <button onClick={incCounter}>I am clicked {count}</button>
-  <span style={ styless}>Hi hello</span>
-  <div></div>
-        </p>
-      </header>
+      Hiiiii {props.name}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./AppClass";
 // import App from "./AppFunction";
-import App from "./register";
+import App from "./App";
 // import App from "./Login";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const username = "Shubham";
+
+ReactDOM.render(<App name={username}/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
