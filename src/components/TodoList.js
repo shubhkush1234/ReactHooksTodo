f2826cecaf90f165d8609a9095a5e4ccd5d83706
shubham kushwaha <shubhkush1234@gmail.com>
2021-07-27 14:52:03 +0530

    added toggle and remove todos features

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import TodosContext from '../context';
 import { useContext } from 'react';
 
 const TodoList = () => {
-    const { state } = useContext(TodosContext);
+    const { state, dispatch } = useContext(TodosContext);
     const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing to do!";
     return (
         <div className="container mx-auto max-w-md text-center font-mono" >
@@ -12,11 +12,13 @@ const TodoList = () => {
             </h1>
             <ul className="list-reset text-white p-0" >
                 {state.todos.map(todo =>
-                    <li key={todo.id} className="flex items-center bg-red-300 border-black border-dashed border-2 my-2 py-4">
-                        <span className="flex-1 ml-12 curser-pointer">{todo.text}</span>
+                    <li key={todo.id} className="flex items-center bg-red-300 border-black
+                        border-dashed border-2 my-2 py-4">
+                        <span className={`flex-1 ml-12 curser-pointer ${todo.complete && "line-through text-gray-900"}`}
+                            onDoubleClick={() => dispatch({type:"TOGGLE_TODO", payload: todo})}> {todo.text}</span>
 
                         <button>Edit</button>
-                        <button>Delete</button>
+                        <button onClick={() => dispatch({type: "REMOVE_TODO", payload: todo})}>Delete</button>
                     </li>)}
             </ul>
         </div>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,15 @@
 export default function reducer (state, action){
     switch(action.type){
+        case "TOGGLE_TODO":
+            const toggledTodos = state.todos.map(t => t.id === action.payload.id ? 
+                { ...action.payload, complete: !action.payload.complete}
+                : t)
+
+            return {...state, todos:toggledTodos}
+        
+        case "REMOVE_TODO":
+                const filteredTodo = state.todos.filter( t => t.id !== action.payload.id)
+            return {...state, todos: filteredTodo}
         default:
             return state;
     }
