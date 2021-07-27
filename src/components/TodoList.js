662f2305fff5afd427074aed79a26a8a09ffc745
shubham kushwaha <shubhkush1234@gmail.com>
2021-07-27 13:46:29 +0530

    todo list basic list view layout done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,12 +4,21 @@ import { useContext } from 'react';
 
 const TodoList = () => {
     const { state } = useContext(TodosContext);
-    return(
+    const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing to do!";
+    return (
         <div className="container mx-auto max-w-md text-center font-mono" >
-            {state.todos.map(todo => 
-                <li key={todo.id}>
-                    <span>{todo.text}</span>
-                </li>)}
+            <h1 className="text-bold">
+                {title}
+            </h1>
+            <ul className="list-reset text-white p-0" >
+                {state.todos.map(todo =>
+                    <li key={todo.id} className="flex items-center bg-red-300 border-black border-dashed border-2 my-2 py-4">
+                        <span className="flex-1 ml-12 curser-pointer">{todo.text}</span>
+
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </li>)}
+            </ul>
         </div>
     )
 }
